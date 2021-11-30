1380a84e48d4177c8ac66f13d353dcadecdbc0fe
ndione gabriel <ndionegabriel@ndiones-MacBook-Pro.fritz.box>
2021-11-30 15:45:47 +0100

    setup signin with google,internet permission,tokenid

--- app/src/main/java/com/example/bookseat/Fragments/LoginFragment.kt
+++ app/src/main/java/com/example/bookseat/Fragments/LoginFragment.kt
@@ -1,16 +1,52 @@
 package com.example.bookseat.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.bookseat.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginFragment : Fragment() {
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+
+    companion object {
+        const val GOOGLE_SIGN_IN = 1903
+    }
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        activityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+            ActivityResultCallback<ActivityResult>() {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+                try {
+                    val account = task.getResult(ApiException::class.java)!!
+                    firebaseAuthWithGoogle(
+                        account.idToken
+                            ?: throw NullPointerException("Expression 'account.idToken' must not be null")
+                    )
+                } catch (e: ApiException) {
+                    //handle error
+                }
+            }
+        )
 
     }
 
@@ -22,7 +58,40 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+        googleSignInClient = GoogleSignIn.getClient(requireContext(), getGSO())
+
+        val button = view.findViewById<SignInButton>(R.id.google_button)
+        button.setOnClickListener { signIn() }
 
     }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        activityResultLauncher.launch(signInIntent)
+    }
+
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    //handle success
+                } else {
+                    //handle error
+                }
+            }
+    }
+
+    private fun getGSO(): GoogleSignInOptions {
+        return GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestIdToken(getString(R.string.webclient_id))
+            .build()
+    }
 }
+
